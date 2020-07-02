019daf3624460577d6feed759b7b71597388f22e
nickamorim <nickamorim98@gmail.com>
2020-07-02 16:40:37 -0400

    share 2 social

--- src/screens/Music.js
+++ src/screens/Music.js
@@ -1,7 +1,26 @@
-import React from 'react';
-import { Button } from '@material-ui/core';
-import Chat from '../components/Chat'
+import React from "react";
+import { Button } from "@material-ui/core";
+import Chat from "../components/Chat";
+import Popup from "reactjs-popup";
+
 // import './styles.css';
+
+import {
+  EmailShareButton,
+  EmailIcon,
+  FacebookIcon,
+  FacebookShareButton,
+  FacebookMessengerIcon,
+  FacebookMessengerShareButton,
+  TumblrIcon,
+  TumblrShareButton,
+  TwitterShareButton,
+  TwitterIcon,
+  RedditIcon,
+  RedditShareButton,
+  WhatsappIcon,
+  WhatsappShareButton,
+} from "react-share";
 
 class Music extends React.Component {
   constructor(props) {
@@ -9,22 +28,59 @@ class Music extends React.Component {
   }
   render() {
     return (
-        <div className="App">
-        <Button onClick={() => this.props.history.push('/')}>Go back to home</Button>
+      <div className="App">
+        <Button onClick={() => this.props.history.push("/")}>
+          Go back to home
+        </Button>
         <p>
-            Welcome to music page.
-            <Button>Share link</Button>
-            <div>
-            Music board goes here
-            </div>
-            <div>
-            Select from sounds/beats
-            </div>
-            <Button>Download Music</Button>
+          Welcome to music page.
+          <div>Music board goes here</div>
+          <div>Select from sounds/beats</div>
+          <Button>Download Music</Button>
         </p>
         <Chat />
-        </div>
-      );
+        <Popup
+          trigger={
+            <button size="lg" className="button">
+              Share Music
+            </button>
+          }
+          modal
+        >
+          {(close) => (
+            <div className="modal">
+              <a className="close" onClick={close}>
+                &times;
+              </a>
+              <div className="header">Share Music to Social Media</div>
+              <div className="content">
+                <FacebookShareButton>
+                  <FacebookIcon size={36} round />
+                </FacebookShareButton>
+                <FacebookMessengerShareButton>
+                  <FacebookMessengerIcon size={36} round />
+                </FacebookMessengerShareButton>
+                <TwitterShareButton>
+                  <TwitterIcon size={36} round />
+                </TwitterShareButton>
+                <TumblrShareButton>
+                  <TumblrIcon size={36} round />
+                </TumblrShareButton>
+                <RedditShareButton>
+                  <RedditIcon size={36} round />
+                </RedditShareButton>
+                <WhatsappShareButton>
+                  <WhatsappIcon size={36} round />
+                </WhatsappShareButton>
+                <EmailShareButton>
+                  <EmailIcon size={36} round />
+                </EmailShareButton>
+              </div>
+            </div>
+          )}
+        </Popup>
+      </div>
+    );
   }
 }
 
