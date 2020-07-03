54e0dc6b775168064c840592da78b307048ffb02
Hyeyeon Shin <hyeyeon.shin@enzuzo.com>
2020-07-03 21:41:48 +0900

    Updates

---
+++ precache-manifest.90b5a113e3c802cf934f31a93ae81b69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03ae9bd6dcd2afc64098442becd44419",
+    "url": "/collabeat/index.html"
+  },
+  {
+    "revision": "2dcaf33a2724fe42d5b7",
+    "url": "/collabeat/static/css/main.fd729e39.chunk.css"
+  },
+  {
+    "revision": "eb1eb378bfce1e3383bd",
+    "url": "/collabeat/static/js/2.a98977ed.chunk.js"
+  },
+  {
+    "revision": "8e82179967c3ba70cae02aea08dd65d1",
+    "url": "/collabeat/static/js/2.a98977ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dcaf33a2724fe42d5b7",
+    "url": "/collabeat/static/js/main.d54050e2.chunk.js"
+  },
+  {
+    "revision": "48690f9d9dcdb7f8e994",
+    "url": "/collabeat/static/js/runtime-main.69d36ca1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3c2759050cfe4201729365684cdf65d2.js"
+  "/collabeat/precache-manifest.90b5a113e3c802cf934f31a93ae81b69.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/collabeat/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
